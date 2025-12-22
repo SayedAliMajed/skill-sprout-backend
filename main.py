ce30b0ca37371f267746d77a0ea05b8ef506d64d
ali <ali.hassan.salman.2000@gmail.com>
2025-12-22 16:30:43 +0300

    add Enrollment_controller

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ import uvicorn
 from contextlib import asynccontextmanager
 
 from controllers.users import router as UserRouter
+from controllers.enrollments import router as EnrollmentRouter
 from database import engine, Base
 from config.environment import ENVIRONMENT, CORS_ORIGINS
 
@@ -55,6 +56,7 @@ app.add_middleware(
 
 # Include routers
 app.include_router(UserRouter, prefix="/api")
+app.include_router(EnrollmentRouter, prefix="/api")
 
 @app.get('/')
 def home():
